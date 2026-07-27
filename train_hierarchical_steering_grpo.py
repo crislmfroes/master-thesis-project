8770766f54d8f27bf22c38c8fd35b9c861536955
crislmfroes <cris.lima.froes@gmail.com>
2026-07-27 14:46:57 -0300

    update

--- train_hierarchical_steering_grpo.py
+++ train_hierarchical_steering_grpo.py
@@ -553,7 +553,7 @@ class LiberoEnv:
             subtask: The subtask to feed into the VLA model.
         """
         prompt = subtask
-        max_steps = 50
+        max_steps = 500
         max_retries = 0
         #self._open_gripper()
         #self.return_to_home_position()
