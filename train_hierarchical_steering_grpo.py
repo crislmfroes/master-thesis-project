4f08e7dcb1c4f61872282fa3633c4750c2d30a58
crislmfroes <cris.lima.froes@gmail.com>
2026-07-30 18:18:59 -0300

    update

--- train_hierarchical_steering_grpo.py
+++ train_hierarchical_steering_grpo.py
@@ -50,12 +50,12 @@ trainer: GRPOTrainer = None
 # ==========================================
 # 1. Configuration & Constants
 # ==========================================
-MODEL_ID = "Qwen/Qwen3.5-4B"
+MODEL_ID = "Qwen/Qwen3.5-0.8B"
 OUTPUT_DIR = "./vlm-grpo-vla-steering"
 
 env_counter = 0
 
-BATCH_SIZE = 4
+BATCH_SIZE = 32
 
 # ==========================================
 # 2. Environment Factory
